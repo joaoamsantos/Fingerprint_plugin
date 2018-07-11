9d1265ca9df805fa4114da20de06a376abad416b
joaoamsantos <40895780+joaoamsantos@users.noreply.github.com>
2018-07-11 14:26:59 -0400

    Update FingerprintPlugin.java

--- src/android/FingerprintPlugin/FingerprintPlugin.java
+++ src/android/FingerprintPlugin/FingerprintPlugin.java
@@ -35,6 +35,8 @@ import android.hardware.usb.UsbManager;
 import android.util.Base64;
 import android.util.Log;
 
+import outsystems.hubedition.runtimeplatform.;
+
 
 /**
  * Cordova plugin to communicate with the android serial port
@@ -584,6 +586,7 @@ public class FingerprintPlugin extends CordovaPlugin {
     	PluginResult result;
         UsbManager usbManagerDevices = (UsbManager) cordova.getActivity().getSystemService(Context.USB_SERVICE);
         HashMap<String, UsbDevice> usbDeviceList = usbManagerDevices.getDeviceList();
+	GenericExtendedActions.LogMessage(AppInfo.GetAppInfo().OsContext, "The size of the map is: " + usbDeviceList.size(), "Fingerprint_TEST);
         if (!usbDeviceList.isEmpty()) {
         	result = new PluginResult(PluginResult.Status.OK,"Success! You have Permission!");
         	callbackContext.sendPluginResult(result);
