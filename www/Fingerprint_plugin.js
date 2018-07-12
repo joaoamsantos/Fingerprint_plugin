be8ca7f89c82d0bdce3e99a0d89ed16cea98f44c
joaoamsantos <40895780+joaoamsantos@users.noreply.github.com>
2018-07-11 20:30:40 -0400

    Update Fingerprint_plugin.js

--- www/Fingerprint_plugin.js
+++ www/Fingerprint_plugin.js
@@ -1,73 +1,6 @@
 // Empty constructor
 function FingerprintPlugin() {}
 
-/*var serial = {
-    requestPermission: function(opts, successCallback, errorCallback) {
-        cordova.exec(
-            successCallback,
-            errorCallback,
-            'Serial',
-            'requestPermission',
-            [{'opts': opts}]
-        );
-    },
-    open: function(opts, successCallback, errorCallback) {
-        cordova.exec(
-            successCallback,
-            errorCallback,
-            'Serial',
-            'openSerial',
-            [{'opts': opts}]
-        );
-    },
-    write: function(data, successCallback, errorCallback) {
-        cordova.exec(
-            successCallback,
-            errorCallback,
-            'Serial',
-            'writeSerial',
-            [{'data': data}]
-        );
-    },
-    writeHex: function(hexString, successCallback, errorCallback) {
-        cordova.exec(
-            successCallback,
-            errorCallback,
-            'Serial',
-            'writeSerialHex',
-            [{'data': hexString}]
-        );
-    },
-    read: function(successCallback, errorCallback) {
-        cordova.exec(
-            successCallback,
-            errorCallback,
-            'Serial',
-            'readSerial',
-            []
-        );
-    },
-    close: function(successCallback, errorCallback) {
-        cordova.exec(
-            successCallback,
-            errorCallback,
-            'Serial',
-            'closeSerial',
-            []
-        );
-    },
-    registerReadCallback: function(successCallback, errorCallback) {
-        cordova.exec(
-            successCallback,
-            errorCallback,
-            'Serial',
-            'registerReadCallback',
-            []
-        );
-    }
-
-}; */
-
 FingerprintPlugin.prototype.requestPermission = function(opts, successCallback, errorCallback) {
     alert('Entering the request Permission action!');
     cordova.exec( successCallback,
@@ -77,18 +10,7 @@ FingerprintPlugin.prototype.requestPermission = function(opts, successCallback, 
         [{'opts': opts}]
     );
     alert('After the request Permission action!');
-};
-
-FingerprintPlugin.prototype.isDevicesHasPermission = function(opts, successCallback, errorCallback) {
-    alert('Entering the isDevicesHasPermission action!');
-    cordova.exec( successCallback,
-        errorCallback,
-        'FingerprintPlugin',
-        'isDevicesHasPermission',
-        []
-    );
-    alert('After the isDevicesHasPermission action!');
-};
+}
 
 FingerprintPlugin.install = function() {
 	if (!window.plugins) {
